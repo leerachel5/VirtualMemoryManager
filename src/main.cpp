0c15590d4868538a297db71438c39a97193d3ad9
Rachel Lee <leerachel481@gmail.com>
2023-03-10 17:47:55 -0800

    Redirect VA translation output to output file

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "FileParser.hpp"
 #include "VMManager.hpp"
 #include <iostream>
+#include <fstream>
 
 
 const std::string INIT_FILE_PATH = "input/init-no-dp.txt";
 const std::string INPUT_FILE_PATH = "input/input-no-dp.txt";
+const std::string OUTPUT_FILE_PATH = "output/output-no-dp.txt";
 
 
 int main() {
@@ -16,10 +18,13 @@ int main() {
     std::vector<int> virtualAddrs;
     FileParser::GetVAInput(INPUT_FILE_PATH, virtualAddrs);
 
-    for (int va : virtualAddrs) {
-        std::cout << vm.translateVAToPA(va) << " ";
+    std::ofstream outputFile(OUTPUT_FILE_PATH);
+    for (int i = 0; i < virtualAddrs.size(); i++) {
+        outputFile << vm.translateVAToPA(virtualAddrs[i]);
+        if (i < virtualAddrs.size() - 1)
+            outputFile << " ";
     }
-    std::cout << std::endl;
+    outputFile.close();
 
     return 0;
 }
